d1ffe48726df5e0374f63d9bf6970fe58387d964
Anthony Graddy <anthony@locationq.com>
2017-01-02 00:47:36 +0000

    Working

---
+++ test/config/index.js
@@ -0,0 +1,14 @@
+var mod = {};
+
+mod.smtp = {};
+
+mod.smtp.accounts = [];
+mod.smtp.accounts.push('basic@example.com');
+mod.smtp.accounts.push('reject@example.com');
+mod.smtp.accounts.push('custom@example.com');
+
+mod.smtp.banner = 'Howdy partner!';
+
+mod.smtp.fqdn = 'mail.example.com';
+
+module.exports = mod;
--- test/index.js
+++ test/index.js
@@ -1,7 +1,153 @@
 var tap = require('agraddy.test.tap')(__filename);
+var events = require('events');
+var net = require('net');
+var path = require('path');
 
-var mod = require('../');
+var emitter = new events.EventEmitter();
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+process.chdir('test');
+
+var smtp = require('../');
+
+var port;
+
+var server = smtp.createServer(function(req, res) {
+	if(req.to == 'basic@example.com') {
+		// Email should come in for one@example.com
+		tap.assert(true, 'Message received.');
+
+		res.accept();
+	}
+	if(req.to == 'reject@example.com') {
+		tap.assert(true, 'Message received.');
+
+		res.reject();
+	}
+	if(req.to == 'custom@example.com') {
+		tap.assert(true, 'Message received.');
+
+		res.write('523 Too large');
+	}
+});
+
+server.on('listening', function() {
+	port = server.address().port;
+
+	basic();
+});
+
+function basic() {
+	runCommands(port, 'basic.txt', noAccount);
+}
+
+function noAccount() {
+	var emitter = runCommands(port, 'no_account.txt', manualRejection);
+
+	emitter.on('data', function(data) {
+		if(data.indexOf('550') !== -1) {
+			tap.assert(true, 'Make sure a 550 response is sent when the email does not have an account.');
+		}
+	});
+}
+
+function manualRejection() {
+	var emitter = runCommands(port, 'reject.txt', custom);
+
+	emitter.on('data', function(data) {
+		if(data.indexOf('550') !== -1) {
+			tap.assert(true, 'Make sure a 550 response is sent when the listener calls res.reject().');
+		}
+	});
+}
+
+function custom() {
+	var emitter = runCommands(port, 'custom.txt', end);
+
+	emitter.on('data', function(data) {
+		if(data.indexOf('523') !== -1) {
+			tap.assert(true, 'Make sure the custom error is sent from res.write().');
+		}
+	});
+}
+
+function end() {
+	tap.assert(true, 'Final function should be called.');
+
+	process.exit();
+}
+
+
+
+
+function getCommands(file) {
+	var fs = require('fs');
+	var commands = new events.EventEmitter();
+	commands.list = [];
+	commands.cont = [];
+
+	fs.readFile(file, function(err, data) {
+		var lines = data.toString().split('\n').filter(function(item) {
+			if(item.slice(0, 1) == '#') {
+				return false;
+			} else {
+				return true;
+			}
+		});
+		var i;
+
+		for(i = 0; i < lines.length; i++) {
+			if(lines[i].slice(0, 2) == '< ') {
+				commands.list.push(lines[i].slice(2) + '\r\n');
+
+				// Check if the command should wait for response or continue
+				// Need to account for comments # Comment maybe filter() the array after the split()
+				if(i > 0 && lines[i - 1].slice(0, 2) == '< ') {
+					commands.cont.push(commands.list.length - 1);
+				}
+			}
+		}
+
+		commands.emit('loaded');
+	});
+
+	return commands;
+}
+
+function runCommands(port, file, cb) {
+	var commands = getCommands(path.join('fixtures', file));
+	var index = 0;
+
+	commands.on('loaded', function() {
+		run();
+	});
+
+	function run() {
+		var client = net.createConnection(port);
+
+		client.on('data', function(data) {
+			console.log('CLIENT RECEIVED: ' + data.toString());
+			commands.emit('data', data.toString());
+			if(index < commands.list.length) {
+				client.write(commands.list[index]);
+			}
+			index++;
+
+			while(commands.cont.indexOf(index) !== -1) {
+				client.write(commands.list[index]);
+				index++;
+			}
+		});
+
+		client.on('end', function(data) {
+			cb();
+		});
+	}
+
+	return commands;
+}
+
+// Listen on an ephemeral port
+server.listen(0, '127.0.0.1');
+
 
 
